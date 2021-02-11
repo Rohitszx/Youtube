1eaae3a86cf58bee5abdda2e528aea95de24c146
rohitszx <rs2001289@gmail.com>
2021-02-12 00:13:01 +0530

    modified

--- src/Recommended.js
+++ src/Recommended.js
@@ -15,31 +15,31 @@ function Recommended() {
                 image="https://ytbassets.s3.ap-south-1.amazonaws.com/Thmb1.jpg"
                 />
                 <VideoCard
-                title="All About Google HashCode"
+                title="Best Place to visit in Switzerland"
                 views="2.3M Views"
-                timestamp="2days ago"
+                timestamp="4days ago"
                 channelImage="###"
-                channel="Rohit Singh"
+                channel="John Doe"
                 image="https://ytbassets.s3.ap-south-1.amazonaws.com/Thmb2.jpg"
                 />
                 <VideoCard
-                title="All About Google HashCode"
-                views="2.3M Views"
+                title="Lake baikal magical ice"
+                views="2.3K Views"
                 timestamp="2days ago"
                 channelImage="###"
                 channel="Rohit Singh"
                 image="https://ytbassets.s3.ap-south-1.amazonaws.com/Thmb3.jpg"
                 />
                 <VideoCard
-                title="All About Google HashCode"
-                views="2.3M Views"
-                timestamp="2days ago"
+                title="IceBath the peace of life"
+                views="5M Views"
+                timestamp="1days ago"
                 channelImage="###"
                 channel="Rohit Singh"
                 image="https://ytbassets.s3.ap-south-1.amazonaws.com/Thmb4.jpg"
                 />
                 <VideoCard
-                title="All About Google HashCode"
+                title="30 days of Code"
                 views="2.3M Views"
                 timestamp="2days ago"
                 channelImage="###"
@@ -47,7 +47,7 @@ function Recommended() {
                 image="https://ytbassets.s3.ap-south-1.amazonaws.com/Thmb5.jpg"
                 />
                 <VideoCard
-                title="All About Google HashCode"
+                title="Vikings - Season 3"
                 views="2.3M Views"
                 timestamp="2days ago"
                 channelImage="###"
